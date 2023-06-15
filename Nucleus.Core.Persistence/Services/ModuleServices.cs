cbdbb84733c4c8f60d899a30458bf026b2479ebe
Matthew Whited <matt.whited@lightwellinc.com>
2023-06-15 18:03:59 -0400

    fix build

--- Nucleus.Core.Persistence/Services/ModuleServices.cs
+++ Nucleus.Core.Persistence/Services/ModuleServices.cs
@@ -1,11 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using Nucleus.Core.Contracts.Models;
 using Nucleus.Core.Contracts.Persistence;
-using Nucleus.Core.Persistence.Collections;
-using Nucleus.Core.Persistence.Models.DbSettings;
-using Nucleus.Core.Shared.Persistence.Services.ServiceHelpers;
 using System.Linq;
 
 namespace Nucleus.Core.Persistence.Services
