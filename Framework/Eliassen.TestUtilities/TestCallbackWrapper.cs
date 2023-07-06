5b0c3914b5831e583bf6b7300f72421c57e44d61
Matthew Whited <matt.whited@lightwellinc.com>
2023-07-06 14:19:32 -0400

    clean up comments and documents

--- Framework/Eliassen.TestUtilities/TestCallbackWrapper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Eliassen.TestUtilities
-{
-    public class TestCallbackWrapper : ITestCallbackWrapper
-    {
-        public TestCallbackHandler? ServiceRequest { get; set; }
-        public TestPublishEvent? PublishEvent { get; set; }
-    }
-}
