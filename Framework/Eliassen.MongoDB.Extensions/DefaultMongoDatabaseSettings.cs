498085924b30b9224dd0c49ff9e526e17b8a6e49
Matthew Whited <matt.whited@lightwellinc.com>
2023-09-06 14:25:23 -0400

    fix pipeline for .Net 7.0 breaking change on dotnet run

--- Framework/Eliassen.MongoDB.Extensions/DefaultMongoDatabaseSettings.cs
+++ Framework/Eliassen.MongoDB.Extensions/DefaultMongoDatabaseSettings.cs
@@ -1,18 +1,23 @@
 ﻿using Eliassen.System.Configuration;
 
-namespace Eliassen.MongoDB.Extensions
-{
-    /// <summary>
-    /// Default connection information for MongoDatabases, Duplicating this class with a different
-    /// ConfigurationSectionAttribute value will allow for declaring secondary connection configurations
-    /// </summary>
-    [ConfigurationSection("MongoDatabase")]
-    public class DefaultMongoDatabaseSettings : IMongoSettings
-    {
-        /// <inheritdoc/>
-        public string ConnectionString { get; set; } = null!;
+namespace Eliassen.MongoDB.Extensions;
 
-        /// <inheritdoc/>
-        public string DatabaseName { get; set; } = null!;
-    }
+/// <summary>
+/// Default connection information for MongoDatabases, Duplicating this class with a different
+/// ConfigurationSectionAttribute value will allow for declaring secondary connection configurations
+/// </summary>
+[ConfigurationSection("MongoDatabase")]
+public class DefaultMongoDatabaseSettings : IMongoSettings
+{
+    /// <inheritdoc/>
+    public string ConnectionString { get; set; } = null!;
+
+    /// <inheritdoc/>
+    public string DatabaseName { get; set; } = null!;
+
+    public override string ToString() => new
+    {
+        ConnectionString,
+        DatabaseName,
+    }.ToString() ?? "";
 }
--- deploy-data.bat
+++ deploy-data.bat
@@ -1,9 +1,33 @@
 
+
+@ECHO OFF
+
+IF '%MongoDatabase__ConnectionString%'=='' SET MongoDatabase__ConnectionString=mongodb://localhost:27017
+
+@ECHO ON
 dotnet run ^
 --project Tools\Nucleus.Dataloader.Cli ^
 --configuration Release ^
+--no-build ^
+--no-restore ^
 -- ^
 --SourcePath=.\Conf\MongoDb\SampleData ^
 --Action=DropCollectionAndImport ^
---ConnectionString=mongodb://localhost:27017 ^
+--ConnectionString=%MongoDatabase__ConnectionString% ^
 --DatabaseName=nucleus
+
+@ECHO OFF
+
+REM dotnet.exe run ^
+REM --project .\tools\Nucleus.Dataloader.Cli ^
+REM --configuration Release ^
+REM --no-build ^
+REM --no-restore ^
+REM --nologo ^
+REM -- ^
+REM --SourcePath=.\Conf\MongoDb\SampleData ^
+REM --Action=Import ^
+REM --DatabaseName=nucleusX
+
+
+REM --no-build --no-restore --nologo ^
