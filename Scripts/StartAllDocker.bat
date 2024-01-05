648c099bad322ef25f8dbaccf1467f88ca14e945
Matthew Whited <mwhited@eliassen.com>
2024-01-05 14:38:12 -0500

    update scripts

--- Scripts/StartAllDocker.bat
+++ Scripts/StartAllDocker.bat
@@ -1,7 +1,11 @@
 
+@ECHO OFF
+
 CALL StartRabbitMQ.bat
 CALL StartAzuriteDocker.bat
 CALL StartSmtp4DevDocker.bat
 CALL StartMongoDbDocker.bat
 
-START http://localhost:3000/
+START http://localhost:7777/
+
+CALL SetupAzurite.bat
--- Scripts/StartSmtp4DevDocker.bat
+++ Scripts/StartSmtp4DevDocker.bat
@@ -1,2 +1,6 @@
 
-docker run -d --name smtp4dev -p 3000:80 -p 25:25 -p 143:143 rnwood/smtp4dev
+
+@ECHO OFF
+
+docker run -d --name smtp4dev -p 7777:80 -p 25:25 -p 143:143 rnwood/smtp4dev
+docker start smtp4dev 
