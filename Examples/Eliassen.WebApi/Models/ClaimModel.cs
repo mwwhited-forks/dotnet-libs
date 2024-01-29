a78d764c7d39fbd42758fb713678e6295419bd4b
Matthew Whited <mwhited@eliassen.com>
2024-01-29 17:51:05 -0500

    add user controller to enumerate user claims

---
+++ Examples/Eliassen.WebApi/Models/ClaimModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Eliassen.WebApi.Models;
+
+public record ClaimModel
+{
+    public required string Issuer { get; init; }
+    public required string Value { get; init; }
+    public required string ValueType { get; init; }
+    public required string Type { get; init; }
+    public required string OriginalIssuer { get; init; }
+
+    public string? SubjectName { get; init; }
+    public string? SubjectLabel { get; init; }
+    public string? SubjectAuthenticationType { get; init; }
+}
