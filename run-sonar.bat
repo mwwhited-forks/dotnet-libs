b99c4d4af9560271a49141334e6cee8283e11e8a
Matthew Whited <mwhited@eliassen.com>
2024-02-02 14:16:01 -0500

    add support for sonar

---
+++ run-sonar.bat
@@ -0,0 +1,12 @@
+
+dotnet tool restore
+
+dotnet sonarscanner begin ^
+/k:"Lightwell-Nucleus_Nucleus.Net.Libs_AY1rEe7JXbPtYw2Rxn60" ^
+/d:sonar.host.url="http://localhost:9000" ^
+/d:sonar.token="sqp_19ee9e7d6345e65dd413660300bbbc596c8ec7ba"
+
+dotnet build
+
+dotnet sonarscanner end ^
+/d:sonar.token="sqp_19ee9e7d6345e65dd413660300bbbc596c8ec7ba"
