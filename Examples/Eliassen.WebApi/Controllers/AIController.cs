884d002b2793b4973ff0776180e7348d1c75d58b
Matthew Whited <mwhited@eliassen.com>
2024-04-19 15:55:02 -0400

    stub out and cleanup example controllers

---
+++ Examples/Eliassen.WebApi/Controllers/AIController.cs
@@ -0,0 +1,36 @@
+﻿using Eliassen.AI;
+using Eliassen.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eliassen.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AIController : ControllerBase
+{
+    private readonly ILangageModelProvider _llmProvider;
+    private readonly IEmbeddingProvider _embedding;
+
+    public AIController(
+        ILangageModelProvider llmProvider,
+        IEmbeddingProvider embedding
+        )
+    {
+        _llmProvider = llmProvider;
+        _embedding = embedding;
+    }
+
+    /// <summary>
+    /// Generate an AbstractAI Response based on the prompt and user input
+    /// </summary>
+    /// <returns>The string response from the AbstractAI</returns>
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<string> GetResponseAsync([FromBody] GenAiRequestModel model) =>
+        await _llmProvider.GetResponseAsync(model.PromptDetails, model.UserInput);
+
+
+    [HttpGet]
+    public async Task<float[]> Embed(string text) => await _embedding.GetEmbeddingAsync(text);
+}
---
+++ Examples/Eliassen.WebApi/Controllers/SearchController.cs
@@ -0,0 +1,48 @@
+﻿using Azure;
+using Eliassen.Search.Models;
+using Eliassen.Search;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Eliassen.WebApi.Controllers;
+
+[Route("[Controller]/[Action]")]
+public class SearchController : Controller
+{
+    private readonly ISearchProvider _search;
+
+    public SearchController(
+        ISearchProvider search
+        )
+    {
+        _search = search;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<SearchResultModel>> List() => await _search.ListAsync();
+
+    [HttpGet]
+    public async Task<IEnumerable<SearchResultWithSummaryModel>> SemanticSearch(string? query = default, int limit = 10)
+    {
+        Response.Headers[$"X-APP-{nameof(query)}"] = query;
+        Response.Headers[$"X-APP-{nameof(limit)}"] = limit.ToString();
+        return await _search.SemanticSearchAsync(query, limit);
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<SearchResultWithSummaryModel>> LexicalSearch(string? query = default, int limit = 10)
+    {
+        Response.Headers[$"X-APP-{nameof(query)}"] = query;
+        Response.Headers[$"X-APP-{nameof(limit)}"] = limit.ToString();
+        return await _search.LexicalSearchAsync(query, limit);
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<SearchResultWithSummaryModel>> HybridSearch(string? query = default, int limit = 10)
+    {
+        Response.Headers[$"X-APP-{nameof(query)}"] = query;
+        Response.Headers[$"X-APP-{nameof(limit)}"] = limit.ToString();
+        return await _search.HybridSearchAsync(query, limit);
+    }
+
+}
