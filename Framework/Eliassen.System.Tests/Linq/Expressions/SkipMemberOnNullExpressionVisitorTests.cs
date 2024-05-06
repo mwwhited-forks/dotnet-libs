4d2d0d61024e620982a42effc762f8782eb266a5
Matthew Whited <mwhited@eliassen.com>
2024-05-06 17:30:15 -0400

    clean up some known issues and improve docs

---
+++ Framework/Eliassen.System.Tests/Linq/Expressions/SkipMemberOnNullExpressionVisitorTests.cs
@@ -0,0 +1,78 @@
+﻿using Eliassen.System.Linq.Expressions;
+using Eliassen.System.Linq.Search;
+using Eliassen.TestUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Eliassen.System.Tests.Linq.Expressions;
+
+[TestClass]
+public class SkipMemberOnNullExpressionVisitorTests
+{
+    public required TestContext TestContext { get; set; }
+
+    [TestMethod]
+    [TestCategory(TestCategories.Unit)]
+    public void NullableCollectionQueryTest_Composite()
+    {
+        var query = new SearchQuery
+        {
+            SearchTerm = "Hello",
+        };
+        this.TestContext.AddResult(query);
+
+        var rawData = new[]
+        {
+            new {Items= (string[]?)["Hello"] , Name=(string?)""},
+            new {Items= (string[]?)[] , Name=(string?)"" },
+            new {Items= (string[]?)null  , Name=(string?)""},
+            new {Items= (string[]?)[]  , Name=(string?)null},
+        }.AsQueryable();
+        this.TestContext.AddResult(rawData);
+
+        var queryResults = QueryBuilder.Execute(rawData, query, new SkipMemberOnNullExpressionVisitor());
+        this.TestContext.AddResult(queryResults);
+    }
+
+    [TestMethod]
+    [TestCategory(TestCategories.Unit)]
+    public void NullableCollectionQueryTest_Array()
+    {
+        var query = new SearchQuery
+        {
+            SearchTerm = "Hello",
+        };
+        this.TestContext.AddResult(query);
+
+        var rawData = new[]
+        {
+                new {Items= (string[]?)[]},
+                 //new {Name=(string?)null},
+        }.AsQueryable();
+        this.TestContext.AddResult(rawData);
+
+        var queryResults = QueryBuilder.Execute(rawData, query, new SkipMemberOnNullExpressionVisitor());
+        this.TestContext.AddResult(queryResults);
+    }
+
+
+    [TestMethod]
+    [TestCategory(TestCategories.Unit)]
+    public void NullableCollectionQueryTest_Element()
+    {
+        var query = new SearchQuery
+        {
+            SearchTerm = "Hello",
+        };
+        this.TestContext.AddResult(query);
+
+        var rawData = new[]
+        {
+             new {Name=(string?)null},
+        }.AsQueryable();
+        this.TestContext.AddResult(rawData);
+
+        var queryResults = QueryBuilder.Execute(rawData, query, new SkipMemberOnNullExpressionVisitor());
+        this.TestContext.AddResult(queryResults);
+    }
+}
