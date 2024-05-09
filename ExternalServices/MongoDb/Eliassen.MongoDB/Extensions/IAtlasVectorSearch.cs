e1fc47958d54138bb3ef73ae97797f779a133b03
Connor Laverty <claverty@eliassen.com>
2024-05-09 15:36:45 +0100

    maybe?

---
+++ ExternalServices/MongoDb/Eliassen.MongoDB/Extensions/IAtlasVectorSearch.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Eliassen.MongoDB.Extensions;
+
+/// <summary>
+/// Provide a centralized means to create the index for Atlas Vector Search
+/// </summary>
+public interface IAtlasVectorSearch
+{
+    /// <summary>
+    /// provide a centralized means to create Atlas Vector Search
+    /// </summary>
+    public string Type { get; set; }
+
+    /// <summary>
+    /// provide a centralized means to create Atlas Vector Search
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// provide a centralized means to create Atlas Vector Search
+    /// </summary>
+    public int NumDimensions { get; set; }
+
+    /// <summary>
+    /// provide a centralized means to create Atlas Vector Search
+    /// </summary>
+    public int Similarity { get; set; }
+}
---
+++ ExternalServices/MongoDb/Eliassen.MongoDB/Extensions/IMongoVectorSearch.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+
+namespace Eliassen.MongoDB.Extensions;
+
+/// <summary>
+/// provide a centralized means to create Atlas Vector Search
+/// </summary>
+public interface IMongoVectorSearch
+{
+    IMongoDatabase CreateVectorSearch<TSettings>()
+        where TSettings : class, IMongoSettings;
+}
